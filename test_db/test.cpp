4777a27ec5bd0fb9cb9d525335ef23f171f0704b
1975352908-cell <1975352908@qq.com>
2025-12-22 18:11:45 +0800

    练习mysql预编译

--- test_db/test.cpp
+++ test_db/test.cpp
@@ -1,7 +1,8 @@
 #include <cinttypes>
 #include <iostream>
+#include <mysql/field_types.h>
 #include <mysql/mysql.h>
-
+#include<string.h>
 MYSQL* mysql_connect()
 {
     MYSQL*conn=mysql_init(nullptr);
@@ -76,10 +77,9 @@ void print(MYSQL_RES*res)
 //练习创建表;
 void test_create(MYSQL*conn)
 {
-    const char*sql=R"SQL(CREATE TABLE IF NOT EXISTS logs(
-        id int primary key auto_increment,
-        level varchar(10),
-        message varchar(20)
+    const char*sql=R"SQL(CREATE TABLE IF NOT EXISTS test_stmt(
+        name varchar(10),
+        age int
     );
     )SQL";
     if(mysql_query(conn,sql)!=0)
@@ -154,17 +154,122 @@ void test_select(MYSQL*conn)
     mysql_free_result(res);
     std::cout<<"select data success"<<std::endl;
 }
-int main()
+//练习更新数据;;
+void test_update(MYSQL*conn)
 {
-    //初始化连接句柄;
-    MYSQL*conn=mysql_connect();
-    //test_create(conn);
-    //test_drop(conn);
-    //test_insert(conn);
-    test_select(conn);
+    //更新表数据
+    const char*sql=R"SQL(update logs set message="update message" where id=1;)SQL";
+}
+//练习删除数据;
+void test_delete(MYSQL*conn)
+{
+    const char*sql=R"SQL(delete from logs where id=1;)SQL";
+    if(!check_sql(conn,sql))
+    {
+        return;
+    }
+    std::cout<<"delete data success"<<std::endl;
 }
 
+//练习mysql的预编译语句;
+void test_stmt(MYSQL*conn)
+{
+    MYSQL_STMT*stmt=mysql_stmt_init(conn);
+    if(stmt==nullptr)
+    {
+        std::cout<<mysql_errno(conn)<<" "<<mysql_error(conn);
+        exit(1);
+    }
+    //初始化成功;
+    const char*sql=R"SQL(insert into test (id,message) values(?,?);)SQL";
 
+    if(mysql_stmt_prepare(stmt, sql, strlen(sql))!=0)
+    {
+        std::cout<<mysql_errno(conn)<<" "<<mysql_error(conn);
+        exit(1);
+    }
+    //开始进行参数绑定：
+    MYSQL_BIND bind[2]={};
+    int id=1;
+    bind[0].buffer_type=MYSQL_TYPE_LONG;
+    bind[0].buffer_length=sizeof(id);
+    bind[0].buffer=&id;
+    //message
+    std::string message="测试消息";
+    bind[1].buffer_type=MYSQL_TYPE_VAR_STRING;
+    bind[1].buffer_length=message.size();
+    bind[1].buffer=(void*)message.c_str();
+    //绑定参数;
+    if(mysql_stmt_bind_param(stmt, bind))
+    {
+        std::cout<<mysql_errno(conn)<<" "<<mysql_error(conn);
+        exit(1);
+    }
+
+}
+void test_stmt_10()
+{
+    MYSQL*conn=mysql_connect();
+    MYSQL_STMT*stmt=mysql_stmt_init(conn);
+    if(stmt==nullptr)
+    {
+        std::cout<<mysql_errno(conn)<<" "<<mysql_error(conn);
+        exit(1);
+    }
+    const char*sql=R"SQL(insert into test (id,message) values(?,?);)SQL";
+    if(mysql_stmt_prepare(stmt,sql,strlen(sql))!=0)
+    {
+        std::cout<<mysql_errno(conn)<<" "<<mysql_error(conn);
+        exit(1);
+    }
+    //开始进行参数绑定：
+    MYSQL_BIND bind[2]={};
+    int id=2;
+    bind[0].buffer_type=MYSQL_TYPE_LONG;
+    bind[0].buffer_length=sizeof(id);
+    bind[0].buffer=&id;
+    //message
+    std::string message="测试消息";
+    bind[1].buffer_type=MYSQL_TYPE_VAR_STRING;
+    bind[1].buffer_length=message.size();
+    bind[1].buffer=(void*)message.c_str();
+    //只绑定一次，修改所指向的值即可;
+    mysql_stmt_bind_param(stmt,bind);
+    //插入十条数据;
+    for(int i=0;i<10;i++)
+    {
+        id+=1;
+        message="测试消息"+std::to_string(i+1);
+        bind[1].buffer_length=message.size();
+        bind[1].buffer=(void*)message.c_str();
+        mysql_stmt_bind_param(stmt,bind);
+        //执行预编译语句;
+        if(mysql_stmt_execute(stmt)!=0)
+        {
+            std::cout<<mysql_errno(conn)<<" "<<mysql_error(conn);
+            exit(1);
+        }
+    }
+    //释放结果集;
+    mysql_stmt_close(stmt);
+    mysql_close(conn);
+    std::cout<<"insert data success"<<std::endl;
+}
+int main()
+{
+//     //初始化连接句柄;
+//     MYSQL*conn=mysql_connect();
+//     //test_create(conn);
+//     //test_drop(conn);
+//     //test_insert(conn);
+//     //test_select(conn);
+//     //test_update(conn);
+//     //test_delete(conn);
+//     test_create(conn);
+//     test_stmt(conn);
+    test_stmt_10();
+    return 0;
+}
 
 
 
